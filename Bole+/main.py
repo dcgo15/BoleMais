d24b8400271854ea94c61a2840eb020d09989da8
Daniel Gomes <76263577+dcgo15@users.noreply.github.com>
2022-06-01 15:12:16 -0300

    Add files via upload

--- Bole+/main.py
+++ Bole+/main.py
@@ -3,24 +3,28 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+class Inicio(Screen):
+    pass
+
+class Calcul(Screen):
+    pass
+
+class Boletim(Screen):
+    pass
+
+class Janela(ScreenManager):
+    pass
+
+kiv = Builder.load_file("janela.kv")
+
 
 class BoleMais(App):
     def build(self):
-        lbl = GridLayout(cols = 1, row_force_default=True, row_default_height=40,
-                         spacing=15, padding = 10)
-        self.label = Label(text="BoleMais")
-        self.nome = TextInput(text="Digite seu nome aqui")
-        
-        self.email = TextInput(text="Digite seu email aqui")
-        submit = Button(text="Salvar", on_press = self.submit)
-        lbl.add_widget(self.label)
-        lbl.add_widget(self.nome)
-        lbl.add_widget(self.email)
-        lbl.add_widget(submit)
-        return lbl
+        return kiv
 
-    def submit(self, obj):
-        print("Nome: ", self.nome.text)
-        print("Email: ", self.email.text)
-
-BoleMais().run()
+if __name__ == "__main__":
+    BoleMais().run()
